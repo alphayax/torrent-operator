57dfbde5ce143d34d97bfe5216b30971bf0f6d57
alphayax <alphayax@gmail.com>
2024-03-07 14:05:53 +0100

    change domain

--- cmd/main.go
+++ cmd/main.go
@@ -100,7 +100,7 @@ func main() {
 		WebhookServer:          webhookServer,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "f90a3480.torrent.alphayax.com",
+		LeaderElectionID:       "f90a3480.alphayax.com",
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
