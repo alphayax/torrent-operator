d2da4a73c2f6601e6e7311a3451cd3a866f188ff
alphayax <alphayax@gmail.com>
2024-03-07 14:08:56 +0100

    Add QBittorrentServer kind

---
+++ api/v1alpha1/qbittorrentserver_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// QBittorrentServerSpec defines the desired state of QBittorrentServer
+type QBittorrentServerSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of QBittorrentServer. Edit qbittorrentserver_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// QBittorrentServerStatus defines the observed state of QBittorrentServer
+type QBittorrentServerStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// QBittorrentServer is the Schema for the qbittorrentservers API
+type QBittorrentServer struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   QBittorrentServerSpec   `json:"spec,omitempty"`
+	Status QBittorrentServerStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// QBittorrentServerList contains a list of QBittorrentServer
+type QBittorrentServerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []QBittorrentServer `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&QBittorrentServer{}, &QBittorrentServerList{})
+}
--- cmd/main.go
+++ cmd/main.go
@@ -129,6 +129,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Torrent")
 		os.Exit(1)
 	}
+	if err = (&controller.QBittorrentServerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "QBittorrentServer")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
