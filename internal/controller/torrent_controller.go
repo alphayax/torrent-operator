d4550516b19fea2cb06b7c2e2a74b51f5918d34b
alphayax <alphayax@gmail.com>
2024-03-14 16:33:31 +0100

    Externalize HumanReadableSize func

--- internal/controller/torrent_controller.go
+++ internal/controller/torrent_controller.go
@@ -181,28 +181,6 @@ func (r *TorrentReconciler) updateTorrentState(ctx context.Context, torrent *tor
 	return r.Status().Update(ctx, torrent)
 }
 
-// HumanReadableSize Convert a size in bytes to a human readable size
-// TODO: Export to another file
-func HumanReadableSize(sizeInit int64) string {
-	// Handle zero size as a special case
-	if sizeInit == 0 {
-		return "0 B"
-	}
-
-	units := []string{"B", "KB", "MB", "GB", "TB", "PB", "EB"}
-	size := float64(sizeInit)
-
-	// Use float division for more accurate calculations
-	index := 0
-	for size >= 1024 && index < len(units)-1 {
-		size = float64(size) / 1024
-		index++
-	}
-
-	// Round the value to one decimal place and format output
-	return fmt.Sprintf("%.3f %s", size, units[index])
-}
-
 func (r *TorrentReconciler) deleteTorrent(ctx context.Context, torrent *torrentv1alpha1.Torrent) error {
 	logger := log.FromContext(ctx)
 	logger.Info("Delete torrent", "Name", torrent.Name)
---
+++ internal/controller/utils.go
@@ -0,0 +1,24 @@
+package controller
+
+import "fmt"
+
+// HumanReadableSize Convert a size in bytes to a human readable size
+func HumanReadableSize(sizeInit int64) string {
+	// Handle zero size as a special case
+	if sizeInit == 0 {
+		return "0 B"
+	}
+
+	units := []string{"B", "KB", "MB", "GB", "TB", "PB", "EB"}
+	size := float64(sizeInit)
+
+	// Use float division for more accurate calculations
+	index := 0
+	for size >= 1024 && index < len(units)-1 {
+		size = float64(size) / 1024
+		index++
+	}
+
+	// Round the value to one decimal place and format output
+	return fmt.Sprintf("%.3f %s", size, units[index])
+}
